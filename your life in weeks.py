1224a540a7799ffe058dd668255086d20f88a8cd
Daksh Shah <135730010+DakshShah555@users.noreply.github.com>
2023-06-07 22:44:24 +0530

    Add files via upload

---
+++ your life in weeks.py
@@ -0,0 +1,9 @@
+years=int(input("Enter your age:"))
+Years_remaining= 90-years
+
+Months=Years_remaining*12
+weeks=Years_remaining*52
+days=Years_remaining*365
+
+
+print(f"Years remaining in your life are {Years_remaining} years, you have {Months} months, you have {weeks} weeks and you have {days} days")
